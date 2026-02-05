b9888b9d35cbe182a879602c204a81e1bd9577e7
Pteraaaaz <barsenrio@gmail.com>
2026-02-05 14:07:23 +0700

    Improving Buy section

--- Static/Js/scripts.js
+++ Static/Js/scripts.js
@@ -1,3 +1,4 @@
+// Menu toggle for phones
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.querySelector('.menu-toggle');
     const nav = document.querySelector('nav');
@@ -7,4 +8,22 @@ document.addEventListener("DOMContentLoaded", () => {
     toggle.addEventListener('click', () => {
         nav.classList.toggle('active');
     });
+});
+
+// For counter feature in productPage
+let count = 0;
+const countDisplay = document.getElementById("count");
+const plusBtn = document.getElementById("plus");
+const minBtn = document.getElementById("minus");
+
+plusBtn.addEventListener("click", () => {
+    count++;
+    countDisplay.textContent = count;
+});
+
+minBtn.addEventListener("click", () => {
+    if (count > 0) {
+        count--;
+        countDisplay.textContent = count;
+    }
 });
